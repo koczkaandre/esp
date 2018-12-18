72399ba65ab9dbb966299764ffcdc8cab111bc42
Andre Koczka <akoczka@student.tugraz.at>
2018-12-18 18:14:23 +0100

    einlesen

--- ass3.c
+++ ass3.c
@@ -13,19 +13,52 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+int verify_config(int);
+char store(char);
 
 int main(int argc, char const *argv[])
 {
-  
-  FILE *f;
-  char c;
-  f=fopen(argv[1],"rt");
+  int num = 0;
+  int index = 0;
+  FILE *file;
+  char line[64];
+  char **buffer;
+  file=fopen(argv[1],"r");
 
-  while((c=fgetc(f))!=EOF)
+  while(fgets(line, sizeof(line), file))
   {
-    printf("%c",c);
+    num++;
   }
-  fclose(f);
+  rewind(file);
+
+  buffer = (char**)malloc(sizeof(line)*sizeof(char));
+
+
+  for(int mem = 0;mem<num;mem++)
+  {
+    buffer[mem] = (char*)malloc(sizeof(line)*sizeof(char));
+  }
+
+  while(fgets(line, sizeof(line), file))
+    {
+      strcpy(buffer[index], line);
+      index++;
+    }
+  
+  for(int x = 0;x<index;x++) printf("%s", buffer[x]);
+
+
+
+
+  free(buffer);
+  fclose(file);
 
   return 0;
 }
+
+
+
+int verify_config(int i);
